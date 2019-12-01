b367c992775b1ac897be19304c0b28af7f5ea7d5
zhuzhenguang <zhuzhenguang2008@gmail.com>
2019-12-01 13:57:16 +0800

    init project

---
+++ Phoenicia.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Phoenicia.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
